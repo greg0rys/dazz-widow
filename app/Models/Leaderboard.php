c99e7f58175f026a90938e97c8e10102b3bd1442
Greg Shenefelt <greg@shenefelt.net>
2026-04-06 11:28:05 -0700

    Update Leaderboard.php

--- app/Models/Leaderboard.php
+++ app/Models/Leaderboard.php
@@ -4,11 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Leaderboard extends Model
 {
     /** @use HasFactory<\Database\Factories\LeaderboardFactory> */
     use HasFactory;
 
-    
+    protected $fillable = [
+        'user_id',
+        'coin_balance',
+        'exp_ponts',
+    ];
+
+    protected $casts = [
+        'coin_balance' => 'integer',
+        'exp_points' => 'integer',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
